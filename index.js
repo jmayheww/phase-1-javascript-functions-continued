7ba8022e3208adbcf1ec7ed3e5fc4f24e115a25e
Joshua <joshsmayhew@gmail.com>
2022-06-09 15:22:54 +0700

    commit tree 817dd79084a4dcac14a77254c1dccd6c22176c28

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair="*") {
+     return function(emphatic= "special") {
+        return `You are ${flair}${emphatic}${flair}!`;
+    };
+}
+
+wrapAdjective()("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
+
